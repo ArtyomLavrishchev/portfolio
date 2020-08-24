c0e713a05da8fe3879cdca9fec39572f426307dc
ArtyomLavrishchev <temalggt540@gmail.com>
2020-08-24 21:11:45 +0300

    first commit

---
+++ src/Main/Main.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import style from "./Main.module.css"
+import styleContainer from "../Common/Styles/Container.module.css"
+function Main() {
+    return (
+        <div className={style.mainBlock}>
+            <div className={styleContainer.container}>
+                <div className={style.text}>
+                    <span>Hi There</span>
+                    <h1>I am Artem Lavrishchev</h1>
+                    <p>Frontend Developer</p>
+                </div>
+                <div className={style.photo}></div>
+            </div>
+        </div>
+    );
+}
+
+export default Main;
