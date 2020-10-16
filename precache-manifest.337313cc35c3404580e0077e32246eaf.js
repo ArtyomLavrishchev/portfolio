978db9a1608a0e6d865d9a7710db8ad46747ad9a
ArtyomLavrishchev <temalggt540@gmail.com>
2020-10-16 23:35:38 +0300

    Updates

---
+++ precache-manifest.337313cc35c3404580e0077e32246eaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a66a7314fc5fc8b0639247a20be4a4c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "10b1fa5bc3e8af4ed0a5",
+    "url": "/portfolio/static/css/main.17ec5bda.chunk.css"
+  },
+  {
+    "revision": "3b4eba872a3479a4d865",
+    "url": "/portfolio/static/js/2.dbe85c02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.dbe85c02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10b1fa5bc3e8af4ed0a5",
+    "url": "/portfolio/static/js/main.f067fa27.chunk.js"
+  },
+  {
+    "revision": "b417b3ee81e3e8f9aad5",
+    "url": "/portfolio/static/js/runtime-main.b649af6b.js"
+  }
+]);
